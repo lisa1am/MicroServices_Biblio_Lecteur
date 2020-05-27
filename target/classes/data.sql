df7e12e7de855ce87769721268381455c1237159
«lisa1am» <laitmouloud@gmail.com>
2020-05-27 23:34:32 +0200

    initial commit

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+insert into Lecteur(id, genre, nom, prenom, date_naissance, adresse) values('1','M','Arzur','Samuel','1997-04-28','9 rue jean moulin');
+insert into Lecteur(id, genre, nom, prenom, date_naissance, adresse) values('2','M','Aït-Mouloud','Lisa','1997-03-30','dans colombe');
+insert into Lecteur(id, genre, nom, prenom, date_naissance, adresse) values('3','M','Arzur','Michel','1963-08-19','9 rue jean moulin');
